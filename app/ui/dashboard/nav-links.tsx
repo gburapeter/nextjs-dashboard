78c583564b91d8afc7e4abf9ca04dbc9e1110219
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2024-04-16 16:23:57 +0200

    initial

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { MotionLink } from './MotionLink';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -15,21 +21,31 @@ const links = [
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
-
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <MotionLink
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.3 },
+            }}
+            whileTap={{ scale: 0.7, transition: { duration: 1 } }}
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </MotionLink>
         );
       })}
     </>
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Permanent_Marker, Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const permanent_marker = Permanent_Marker({
+  weight: ['400'],
+  subsets: ['latin'],
+});
